fac87bb15b05c6bb34c0312d5033dc2b027aa1ce
Avvakumov, Dmitrii A <Avvakumov.DA@parma.ru>
2023-07-31 16:12:38 +0500

    fixed: исправления GuestPageService

--- src/main/java/ru/parma/filesdistr/dto/GuestPageDto.java
+++ src/main/java/ru/parma/filesdistr/dto/GuestPageDto.java
@@ -2,13 +2,10 @@ package ru.parma.filesdistr.dto;
 
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 public class GuestPageDto {
 
-    private String versionNumber;
-    private List<FolderDto> folders;
+    private ScopeDto scopeDto;
     //TODO !!!не забыть добавить, если необходимо!!!
 }
--- src/main/java/ru/parma/filesdistr/service/GuestPageService.java
+++ src/main/java/ru/parma/filesdistr/service/GuestPageService.java
@@ -3,14 +3,15 @@ package ru.parma.filesdistr.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.parma.filesdistr.dto.GuestPageDto;
-import ru.parma.filesdistr.mappers.FolderMapper;
+import ru.parma.filesdistr.mappers.ScopeMapper;
+import ru.parma.filesdistr.models.Folder;
 import ru.parma.filesdistr.models.Scope;
 import ru.parma.filesdistr.models.Version;
 import ru.parma.filesdistr.repos.ScopeRepository;
 import ru.parma.filesdistr.repos.VersionRepository;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -19,19 +20,42 @@ public class GuestPageService {
 
     private final ScopeRepository scopeRepository;
 
+    private final VersionRepository versionRepository;
 
     public GuestPageDto getGuestPageByScopeId(Long scopeId) {
-        Scope scope = scopeRepository.getScopeByScopeIdAndLatestVersion(scopeId);
+        Scope scope = scopeRepository.getReferenceById(scopeId);
         if (scope==null) {
             throw new EntityNotFoundException("Scope с таким id не найден");
+        }
+        List<Folder> folders = scope.getFolders();
+        for (Folder folder : folders) {
+            List<Version> versions = folder.getVersions();
+            Version versionWithMaxId = versions.stream().max(Comparator.comparing(v -> v.getId())).get();
+            versions.clear();
+            versions.add(versionWithMaxId);
         }
         return convertEntityToDto(scope);
     }
 
     public GuestPageDto getGuestPageByVersionId(Long versionId) {
-        Scope scope = scopeRepository.getScopeByVersionId(versionId);
-        if (scope==null) {
-            throw new EntityNotFoundException("Scope с такой версией не найден");
+        Version versionWithId = versionRepository.getReferenceById(versionId);
+        if (versionWithId==null) {
+            throw new EntityNotFoundException("Version с таким id не найден");
+        }
+        String versionNumber = versionWithId.getVersionNumber();
+        Folder folderWithVersion = versionWithId.getFolder();
+        Scope scope = folderWithVersion.getScope();
+        List<Folder> folders = scope.getFolders();
+        for (Folder folder : folders) {
+            List<Version> versions = folder.getVersions();
+            Version version = null;
+            for (Version tmp : versions) {
+                if (tmp.getVersionNumber().equals(versionNumber)) {
+                    version = tmp;
+                }
+            }
+            versions.clear();
+            versions.add(version);
         }
         return convertEntityToDto(scope);
     }
@@ -39,12 +63,7 @@ public class GuestPageService {
     private GuestPageDto convertEntityToDto(Scope scope) {
         GuestPageDto guestPageDto = new GuestPageDto();
 
-        //Переделка через mapper
-        guestPageDto.setFolders(FolderMapper.INSTANCE.toFolderDtos(scope.getFolders()));
-
-        //Задание версии
-        Version version = scope.getFolders().get(0).getVersions().get(0);
-        guestPageDto.setVersionNumber(version.getVersionNumber());
+       guestPageDto.setScopeDto(ScopeMapper.INSTANCE.toScopeDto(scope));
 
         //TODO !!!не забыть добавить, если необходимо!!!
         return guestPageDto;
