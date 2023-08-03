e2f62e98e5ee7bd8fcade75a3a3ae404958cf363
stormdesso <stormdesso@gmail.com>
2023-08-03 13:40:19 +0500

    подтянул изменения из main

--- src/main/java/ru/parma/filesdistr/controllers/GuestPageController.java
+++ src/main/java/ru/parma/filesdistr/controllers/GuestPageController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import ru.parma.filesdistr.dto.GuestPageDto;
 import ru.parma.filesdistr.enums.TypeInScopePage;
+import ru.parma.filesdistr.service.CustomUserDetailsService;
 import ru.parma.filesdistr.service.GuestPageService;
 import ru.parma.filesdistr.service.ScopeAccessService;
 
@@ -22,23 +23,18 @@ public class GuestPageController {
 
     private final ScopeAccessService scopeAccessService;
 
-    static int userId = 2;
 
     @GetMapping("v/{version_id}")
     @ResponseBody
     public GuestPageDto getGuestPageByVersionId(@PathVariable Long version_id) throws AccessDeniedException {
-        if (scopeAccessService.tryGetAccess(TypeInScopePage.VERSION, version_id, userId)) {
-            return guestPageService.getGuestPageByVersionId( version_id);
-        }
-        else throw new AccessDeniedException("Access denied");
+        scopeAccessService.tryGetAccess(TypeInScopePage.VERSION, version_id, CustomUserDetailsService.getAuthorizedUserId());
+        return guestPageService.getGuestPageByVersionId( version_id);
     }
 
     @GetMapping("s/{scope_id}")
     @ResponseBody
     public GuestPageDto getGuestPageByScopeId(@PathVariable Long scope_id) throws AccessDeniedException {
-        if (scopeAccessService.tryGetAccess(TypeInScopePage.SCOPE, scope_id, userId)) {
-            return guestPageService.getGuestPageByScopeId( scope_id);
-        }
-        else throw new AccessDeniedException("Access denied");
+        scopeAccessService.tryGetAccess(TypeInScopePage.SCOPE, scope_id, CustomUserDetailsService.getAuthorizedUserId());
+        return guestPageService.getGuestPageByScopeId( scope_id);
     }
 }
