de25b9ac5a75344caae37a33e55a8a1b0d72346c
stormdesso <stormdesso@gmail.com>
2023-08-04 12:12:07 +0500

    added: настройки для выгрузки на стенд

--- src/main/java/ru/parma/filesdistr/Application.java
+++ src/main/java/ru/parma/filesdistr/Application.java
@@ -2,16 +2,24 @@ package ru.parma.filesdistr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 import ru.parma.filesdistr.config.WebSecurityConfig;
 
 @SpringBootApplication
 @Import(WebSecurityConfig.class)
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(Application.class);
+    }
 
 }
+
+
 
