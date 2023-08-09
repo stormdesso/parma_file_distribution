46aa4cdf79f10fd48c2944df6204ea68cccc50cc
stormdesso <stormdesso@gmail.com>
2023-08-09 19:22:16 +0500

    #1154 added: метод получения дерева

--- src/main/java/ru/parma/filesdistr/controllers/UsersController.java
+++ src/main/java/ru/parma/filesdistr/controllers/UsersController.java
@@ -1,79 +1,81 @@
 package ru.parma.filesdistr.controllers;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.parma.filesdistr.dto.AdminDto;
 import ru.parma.filesdistr.dto.AdminScopeDto;
+import ru.parma.filesdistr.enums.Roles;
+import ru.parma.filesdistr.models.User;
+import ru.parma.filesdistr.repos.UserRepository;
+import ru.parma.filesdistr.service.CustomUserDetailsService;
 import ru.parma.filesdistr.service.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/users")
 public class UsersController {
     private final UserService userService;
+    private final UserRepository userRepository;
+
     @GetMapping("/admin/all")
     @ResponseBody
-    public List<AdminDto> getAllAdmins() {
-        return new ArrayList<>();
+    public Set<User> getTree () {
+        Set<User> users = new HashSet<>();
+        Long id = CustomUserDetailsService.getAuthorizedUserId();
+        User currUser = userRepository.getReferenceById(id);
+        Set<Roles> rolesSet = currUser.getRoles();
+
+        if(rolesSet.contains(Roles.ROOT)){
+            users.addAll(userRepository.findByIdIsNot(id));
+        }
+        else
+            if(rolesSet.contains(Roles.ADMIN)){
+                if(currUser.isAdminManager()){
+                    users.addAll(userRepository.findByIdIsNotAndAdminManagerIsTrue(id));
+                }
+                if(currUser.isAdminScopeManager()){
+                    users.addAll(userRepository.findByIdIsNotAndAdminScopeManagerIsTrue(id));
+                }
+        }
+
+        return users;
     }
 
-    @GetMapping("/admin_scope/all")
-    @ResponseBody
-    public AdminScopeDto getAllAdminsScopes() {
-        return new AdminScopeDto();
-    }
-
-    @GetMapping("/admin/{admin_id}")
-    @ResponseBody
-    public AdminDto get(@PathVariable String admin_id) {
-        return new AdminDto();
-    }
-
-    @GetMapping("/admin_scope/{admin_scope_id}")
-    @ResponseBody
-    public AdminScopeDto getAdminScope( @PathVariable Long admin_scope_id) {
-        return new AdminScopeDto();
-    }
 
     @PutMapping("/admin/update")
-    public void update(@RequestBody AdminDto adminDto)
-    {
+    public void update (@RequestBody AdminDto adminDto) {
         //System.out.println("Изменения сохранены");
     }
 
     @PostMapping("/admin/add")
-    public void add (@RequestBody AdminDto adminDto)
-    {
+    public void add (@RequestBody AdminDto adminDto) {
         //System.out.println("Пространство добавлено");
 
     }
 
     @DeleteMapping("/admin/delete/{admin_id}")
-    public void deleteAdmin( @PathVariable Long admin_id)
-    {
+    public void deleteAdmin (@PathVariable Long admin_id) {
         //System.out.println("Пространство удалено");
     }
 
     @PutMapping("/admin_scope/update")
-    public void update(@RequestBody AdminScopeDto adminScopeDto)
-    {
+    public void update (@RequestBody AdminScopeDto adminScopeDto) {
         //System.out.println("Изменения сохранены");
     }
 
     @PostMapping("/admin_scope/add")
-    public void add (@RequestBody AdminScopeDto adminScopeDto)
-    {
+    public void add (@RequestBody AdminScopeDto adminScopeDto) {
         //System.out.println("Пространство добавлено");
 
     }
 
     @DeleteMapping("/admin_scope/delete/{admin_scope_id}")
-    public void deleteAdminScope( @PathVariable Long admin_scope_id)
-    {
+    public void deleteAdminScope (@PathVariable Long admin_scope_id) {
         //System.out.println("Пространство удалено");
     }
 }
--- src/main/java/ru/parma/filesdistr/repos/UserRepository.java
+++ src/main/java/ru/parma/filesdistr/repos/UserRepository.java
@@ -1,10 +1,28 @@
 package ru.parma.filesdistr.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.parma.filesdistr.models.User;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByName(String name);
+    @Query(value = "SELECT * FROM users " +
+                        "WHERE (id != ?1 AND (is_admin_scope_manager = TRUE OR is_admin_manager = TRUE))",
+            nativeQuery = true)
+
+    List<User> findByIdIsNot(Long id);
+
+    @Query(value = "SELECT * FROM users  " +
+            "WHERE (id != ?1 AND is_admin_manager = TRUE)",
+            nativeQuery = true)
+    List<User> findByIdIsNotAndAdminManagerIsTrue(Long id);
+
+    @Query(value = "SELECT * FROM users  " +
+                        "WHERE (id != ?1 AND is_admin_scope_manager = TRUE)",
+            nativeQuery = true)
+    List<User> findByIdIsNotAndAdminScopeManagerIsTrue(Long id);
 }
