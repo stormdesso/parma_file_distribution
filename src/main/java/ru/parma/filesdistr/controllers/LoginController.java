c6b924d6e0cee262022e52c7f317467ace945c62
stormdesso <stormdesso@gmail.com>
2023-08-22 16:51:08 +0500

    add: getRoles

--- src/main/java/ru/parma/filesdistr/controllers/LoginController.java
+++ src/main/java/ru/parma/filesdistr/controllers/LoginController.java
@@ -4,18 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.parma.filesdistr.enums.Roles;
+import ru.parma.filesdistr.models.User;
+import ru.parma.filesdistr.service.CustomUserDetailsService;
+
+import java.util.Set;
 
 
 @RestController
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-    @GetMapping("/logout")
-    public String logout() {
-        return "logout";
+    @GetMapping("/get_roles")
+    public Set<Roles> getRoles() {
+        if(CustomUserDetailsService.isAuthenticated()) {
+            return ((User) (CustomUserDetailsService.getAuthentication().getPrincipal())).getRoles();
+        }
+        else
+            return null;
     }
 }
